cdd6253ea8b365b618278d3fd64b58581cf03f2d
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2024-06-13 16:04:56 -0600

    feat: add users table

--- hostelry.sql
+++ hostelry.sql
@@ -16,3 +16,11 @@ CREATE TABLE extras (
     description DECIMAL(5,2) NOT NULL,
     price DECIMAL(6,2) NOT NULL 
 );
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(15) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    role ENUM('user', 'admin') DEFAULT 'user'
+);
