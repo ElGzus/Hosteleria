732007b7ef509a0ea1d9aba07a974679339e4dcc
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2024-06-20 12:01:54 -0600

    feat: modify login to match with username and passowrd with other query and fetching data

--- models/User.php
+++ models/User.php
@@ -30,23 +30,33 @@
       if ($statement->execute()) {
         return true;
       }
-
       return false;
     }
 
     public function login() {
-      $query = "SELECT id, username, email, role FROM " . $this->table_name . " WHERE username=:username AND password=:password";
+      // query to check if username exists, the ? is a placeholder for the actual value and LIMIT 0,1 is to get only one record
+      $query = "SELECT * FROM " . $this->table_name . "WHERE username = ? LIMIT 0,1";
       $statement = $this->connection->prepare($query);
-
-      $this->username = htmlspecialchars(strip_tags($this->username));
-      $this->password = htmlspecialchars(strip_tags($this->password));
-
-      $statement->bindParam(":username", $this->username);
-      $statement->bindParam(":password", $this->password);
-
+      // bind value to the placeholder
+      $statement->bindParam(1, $this->username);
       $statement->execute();
 
-      return $statement;
+      // fetch the record with an associative array which is a PDO built-in method
+      $record = $statement->fetch(PDO::FETCH_ASSOC);
+      if ($record) {
+        // if the record is found, assign the values from the database to the object's properties
+        // Assign in the sense of assigning the values from the database to the object's properties no to the database (to be very clear)
+        // the password_verify() method is used to verify if the password matches the hashed password in the database
+        if (password_verify($this->password, $record["password"])) {
+          $this->id = $record["id"];
+          $this->username = $record["username"];
+          $this->email = $record["email"];
+          $this->role = $record["role"];
+          return true;
+        }
+      }
+      // return false if the record is not found or the password is incorrect
+      return false;
     }
   }
 ?>
