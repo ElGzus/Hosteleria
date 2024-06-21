5ac1b5ccf9594844431661a17bec164c4388551f
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2024-06-21 16:47:50 -0600

    feat: add admin form to add accommodations

---
+++ views/admin.php
@@ -0,0 +1,14 @@
+<?php include "templates/header.php"; ?>
+
+<h1>Add accommodations</h1>
+<form action="" method="POST">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name">
+    <label for="description">Description</label>
+    <input type="text" name="description" id="description">
+    <label for="location">Location</label>
+    <input type="text" name="location" id="location">
+    <label for="image">Image</label>
+    <input type="text" name="image" id="image">
+    <input type="submit" value="Add">
+</form>
